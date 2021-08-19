493a83242dd30b2835e8ef70f64a84fec1fdddf1
unknown <bil.konstantinova@gmail.com>
2021-08-19 19:02:05 +0300

    added whitenoise

--- artify/settings.py
+++ artify/settings.py
@@ -37,7 +37,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     # 'responsive.middleware.DeviceInfoMiddleware',
 ]
 
